bc5ae594cf17a80ae36758386621aa1a7ddbf829
dudj <13020078873@163.com>
2019-08-16 15:33:48 +0800

    将超时配置到配置文件，注意默认的key就是方法名

--- order/server/src/main/java/com/linziyi/order/controller/HystrixController.java
+++ order/server/src/main/java/com/linziyi/order/controller/HystrixController.java
@@ -4,6 +4,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,11 +13,23 @@ import java.util.Arrays;
 @RestController
 @DefaultProperties(defaultFallback = "defaultFallback")
 public class HystrixController {
-    @HystrixCommand(commandProperties = {
+    //Hystrix 的 超时配置
+    /*@HystrixCommand(commandProperties = {
             @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value = "3000")
-    })
+    })*/
+    //熔断
+    /*@HystrixCommand(commandProperties = {
+            @HystrixProperty(name="circuitBreaker.enabled",value = "true"),
+            @HystrixProperty(name="circuitBreaker.requestVolumeThreshold",value = "10"),
+            @HystrixProperty(name="circuitBreaker.sleepWindowInMilliseconds",value = "10000"),
+            @HystrixProperty(name="circuitBreaker.errorThresholdPercentage",value = "60"),
+    })*/
+    @HystrixCommand
     @RequestMapping("/getProductInfoList")
-    public String getProductInfoList(){
+    public String getProductInfoList(@RequestParam("number") Integer number){
+        if(number % 2 ==0){
+            return "success";
+        }
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.postForObject("http://localhost:8060/product/listForId",
                 Arrays.asList("157875196366160022"),
